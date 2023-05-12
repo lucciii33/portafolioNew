5992b87c50bdedd8fb89e27e8c56deac8fbf77db
Angelo <angelo@Angelos-MacBook-Pro.local>
2023-05-11 20:02:54 -0400

    new component

---
+++ src/components/workCard.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+
+import "../projects.css";
+
+function WorkCard({title, description, time}) {
+
+
+  return (
+    <>
+      <div className="" >
+        <div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+
+        <div className="workCard-image">
+            <p>Image here</p>
+        </div>
+
+        <div className="flex-2">
+            <div>
+                <h2>timeline</h2>
+                <p>{time}</p>
+                <h2>Lenguajes</h2>
+                <p>Next.js, chakraUi, <br/>redux, express, node.js mongodb</p>
+            </div>
+
+            <div className="max-width">
+            <h2>overview</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse  cillum dolore eu fugiat nulla pariatur. </p>
+            <p>check project</p>
+            </div>
+
+        </div>
+       <br/>
+       <br/>
+       <br/>
+      </div>
+    </>
+  );
+}
+
+export default WorkCard;
--- src/projects.jsx
+++ src/projects.jsx
@@ -4,13 +4,19 @@ import email from "./assets/email.png";
 import arrow from "./assets/arrow.png";
 import viteLogo from "/vite.svg";
 import "./projects.css";
+import WorkCard from "./components/workCard";
 
 function Projects() {
-  const [count, setCount] = useState(0);
+
 
   return (
     <>
       <div className="projects-main" id="projects-main">
+        <div>
+        <WorkCard title={"test"} description={"A medical Software to help tracking patiens"} time={"12 Months"} frontend={"react.js, Bootstrap, MDBootstrap"} backend={"Express.js, node.js, mongoDB"}/>
+        <WorkCard title={"test2"} description={"Line5 a finacial services the best option to loan your home"} time={"4 month"}/>
+        </div>
+     
        
       </div>
     </>
