401bcb5cfb731b3a9ecbc106ff139953fe65034a
Angelo <angelo@Angelos-MacBook-Pro.local>
2023-06-27 11:07:24 -0400

    more work new images

--- src/projects.jsx
+++ src/projects.jsx
@@ -2,8 +2,9 @@ import { useState,useEffect, useRef } from "react";
 import hand from "./assets/victory.png";
 import email from "./assets/email.png";
 import arrow from "./assets/arrow.png";
-import mockup1 from "./assets/mockup1.jpg";
-import mockup2 from "./assets/mockup2.jpg";
+import mockup1 from "./assets/BlueMedical.png";
+import mockup2 from "./assets/BluelightTech.png";
+import mockup3 from "./assets/Elana.png";
 import viteLogo from "/vite.svg";
 import "./projects.css";
 import WorkCard from "./components/workCard";
@@ -17,8 +18,8 @@ function Projects({scrollPosition}) {
       <div  className='projects-main' id="projects-main">
         <div>
         <WorkCard title={"Blue Medical"} description={"A medical Software to help tracking patiens"} time={"12 Months"} frontend={"React.js, Bootstrap, MDBootstrap, Redux"} backend={"Express.js, node.js, mongoDB"} mockup1={mockup2} link={"https://www.bluelighttech.co"} scrollPosition={scrollPosition} />
-        <WorkCard title={"Elana"} description={"Line5 a finacial services the best option to loan your home"} time={"4 month"}frontend={"Next.js, ChakraUI, Redux"} backend={"Express.js, node.js, mongoDB"}  mockup1={mockup1} link={"https://staging-site-elana.netlify.app"} scrollPosition={scrollPosition}  />
-        <WorkCard title={"Elana"} description={"Line5 a finacial services the best option to loan your home"} time={"4 month"}frontend={"Next.js, ChakraUI, Redux"} backend={"Express.js, node.js, mongoDB"}  mockup1={mockup1} link={"https://staging-site-elana.netlify.app"} scrollPosition={scrollPosition}  />
+        <WorkCard title={"Blue light Tech"} description={"Line5 a finacial services the best option to loan your home"} time={"4 month"}frontend={"Next.js, ChakraUI, Redux"} backend={"Express.js, node.js, mongoDB"}  mockup1={mockup1} link={"https://staging-site-elana.netlify.app"} scrollPosition={scrollPosition}  />
+        <WorkCard title={"Elana"} description={"Line5 a finacial services the best option to loan your home"} time={"4 month"}frontend={"Next.js, ChakraUI, Redux"} backend={"Express.js, node.js, mongoDB"}  mockup1={mockup3} link={"https://staging-site-elana.netlify.app"} scrollPosition={scrollPosition}  />
         </div>
     
       </div>
