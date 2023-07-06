b67d803762e470bf5f6b1265f08872ac651b7e7c
Angelo Maiele <angelomaiele@gamil.com>
2023-07-06 10:52:27 -0400

    video working now

--- src/components/coffeBanner.jsx
+++ src/components/coffeBanner.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect, useRef } from "react";
-import coffee from "../assets/coffee-mug.png"
+import coffee from "../assets/coffee-mug.png";
+import ReactPlayer from 'react-player'
 
 import "../coffe.css";
 
-function CoffeBanner({scrollPosition}) {
-
-  const [popUp, setPopUp] = useState(false)
+function CoffeBanner({ scrollPosition }) {
+  const [popUp, setPopUp] = useState(false);
   const videoUrl = `https://www.youtube.com/watch?v=M_iCO6DV0_c`;
 
   const boxesRef = useRef([]);
@@ -14,20 +14,23 @@ function CoffeBanner({scrollPosition}) {
     const handleIntersection = (entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('animate');
+          entry.target.classList.add("animate");
         } else {
-          entry.target.classList.remove('animate');
+          entry.target.classList.remove("animate");
         }
       });
     };
 
     const observerOptions = {
       root: null,
-      rootMargin: '0px',
+      rootMargin: "0px",
       threshold: 0.8,
     };
 
-    const observer = new IntersectionObserver(handleIntersection, observerOptions);
+    const observer = new IntersectionObserver(
+      handleIntersection,
+      observerOptions
+    );
 
     boxesRef.current.forEach((box) => {
       observer.observe(box);
@@ -38,31 +41,54 @@ function CoffeBanner({scrollPosition}) {
     };
   }, []);
 
-
   return (
     <>
-      <div className="container-coffe" >
-      <img src={coffee} className="coffee-img"></img>
-        <h3 className="pa" ref={(el) => (boxesRef.current[0] = el)}>Let's chat.</h3>
-        <p className="text1 pa" ref={(el) => (boxesRef.current[1] = el)}>want to see more work?<br  className="d-mobile"/> talk about code?<br/>
-        tell me all about the <br className="d-mobile"/>latest conspiracy theories?</p>
-       
-        <a href="https://github.com/lucciii33" target="_blank"  className={scrollPosition < 2 ? "a_tag-decoration pa" : "a_tag-decoration2 pa"} ref={(el) => (boxesRef.current[2] = el)}><span className="separation">Github</span></a>
-        <a href="https://www.linkedin.com/in/angelomaiele/" target="_blank" className={scrollPosition < 2 ? "a_tag-decoration pa" : "a_tag-decoration2 pa"} ref={(el) => (boxesRef.current[3] = el)}><span>Linkedin</span></a>
+      <div className="container-coffe">
+        <img src={coffee} className="coffee-img"></img>
+        <h3 className="pa" ref={(el) => (boxesRef.current[0] = el)}>
+          Let's chat.
+        </h3>
+        <p className="text1 pa" ref={(el) => (boxesRef.current[1] = el)}>
+          want to see more work?
+          <br className="d-mobile" /> talk about code?
+          <br />
+          tell me all about the <br className="d-mobile" />
+          latest conspiracy theories?
+        </p>
+
+        <a
+          href="https://github.com/lucciii33"
+          target="_blank"
+          className={
+            scrollPosition < 2 ? "a_tag-decoration pa" : "a_tag-decoration2 pa"
+          }
+          ref={(el) => (boxesRef.current[2] = el)}
+        >
+          <span className="separation">Github</span>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/angelomaiele/"
+          target="_blank"
+          className={
+            scrollPosition < 2 ? "a_tag-decoration pa" : "a_tag-decoration2 pa"
+          }
+          ref={(el) => (boxesRef.current[3] = el)}
+        >
+          <span>Linkedin</span>
+        </a>
         <p onClick={() => setPopUp(!popUp)}>Last news</p>
 
-        {popUp ? <div className="popup">
-          <div className="modal">
-          <iframe
-          className="frame"
-        title="YouTube video player"
-        width="560"
-        height="350"
-        src={videoUrl}
-        allowFullScreen
-      ></iframe>
+        {popUp ? (
+          <div className="popup">
+            
+            <div className="modal">
+            <div className="xelement" onClick={() => setPopUp(false)}>X</div>
+              <ReactPlayer url={videoUrl} />
+            </div>
           </div>
-        </div> : ""}
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
